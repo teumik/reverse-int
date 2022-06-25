802ba887569d172add03cc3ded91d06941ff253e
Artem Mokhov <teumik@gmail.com>
2022-06-26 01:54:45 +0400

    feat: add reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if (n > 9999999999999998) {
+        return 'Please, enter a number less than 9999999999999998! JS can\'t do this...'
+    }
+    const stringNumber = Math.abs(n).toString();
+    let result = '';
+    for (let i = 0; i < stringNumber.length; i++) {
+        result = `${stringNumber[i]}${result}`;
+    }
+    /* 
+    // Break commit in case of... if you want something spicy!
+    if (n.toString()[0] === '-') {
+        result = `${'-'}${result}`;
+    }
+    */
+    return result;
 }
